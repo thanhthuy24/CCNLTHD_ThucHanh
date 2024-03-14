5207c334b00c80a4af47f4fb893b78b54c8ff7e4
hothanhthuy11.2003@gmail.com <hothanhthuy11.2003@gmail.com>
2024-03-14 11:40:07 +0700

    db

---
+++ ecourseapi/courses/admin.py
@@ -0,0 +1,39 @@
+import cloudinary
+from django.contrib import admin
+from courses.models import Category, Course
+from django.utils.html import mark_safe
+from django import forms
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+
+
+class CourseForm(forms.ModelForm):
+    description = forms.CharField(widget=CKEditorUploadingWidget)
+
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'active', 'created_date', 'updated_date']
+    search_fields = ['name']
+    list_filter = ['id', 'name', 'created_date']
+    readonly_fields = ['my_image']
+    form = CourseForm
+
+    def my_image(self, course):
+        if course.image:
+            if type(course.image) is cloudinary.CloudinaryResource:
+                return mark_safe(f"<img width='300' src='{course.image.url}' />")
+        return mark_safe(f"<img width='300' src='/static/{course.image.name}' />")
+
+    class Media:
+        css = {
+            'all': ['/static/css/style.css']
+        }
+
+admin.site.register(Category)
+admin.site.register(Course, CourseAdmin)
+
+
